9fca7081cfa79efe1e431c3ef4149483ff0d57c9
Kelly <kellyjp09@gmail.com>
2020-04-24 15:04:29 -0500

    Adds JUnit test class and methods

---
+++ src/test/java/FarmTest.java
@@ -0,0 +1,34 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class FarmTest {
+
+
+    @BeforeClass
+    public static void setUp() {
+
+    }
+
+    @AfterClass
+    public static void tearDown() {
+
+    }
+
+    @Test
+    public void StrategyTest() {
+
+    }
+
+    @Test
+    public void FactoryTest() {
+
+    }
+
+    @Test
+    public void ObserverTest() {
+
+    }
+}
